4910f251137b0c24a3760a7b3db439a6623402d0
María José Bravo Macías <majitobravomacias@gmail.com>
2014-06-26 09:30:10 -0500

    Catalogo

---
+++ catalogo.php
@@ -0,0 +1,53 @@
+<?php
+echo <<< HERE
+<div class="catalogo">
+	<h3>Cat&aacute;logo</h3></br>
+	<div id="row1" align="center">
+		<div id="column2">
+			<img src = "Catalogo/Images cat1/fallen.jpg" width="40%" 	height="20%"></img></br>Lauren Kate
+		</div>
+		<div id="column3">
+			<img src = "Catalogo/Images cat1/256683.jpg" 		width="40%" 	height="20%"></img></br>Cassandra Clare
+		</div>
+		<div id="column4">
+			<img src = "Catalogo/Images cat1/11870085.jpg" width="40%" height="20%"></img></br>John Green
+		</div>
+		<div id="column5">
+			<img src = "Catalogo/Images cat1/dark-angel-	lisa-jane-smith-L-1.jpeg" width="40%" height="20%"></img></br>L. J. Smith
+			</div>
+		<div id="column6">
+			<img src = "Catalogo/Images cat1/large_Pretty_When_She_Destroys.jpg" width="40%" height="20%"></img></br>Rhiannon Frater
+		</div>
+	</div>
+	<div id="row2" align="center">
+		<div id="column2">
+			<img src = "Catalogo/Images cat2/100000-Kingdoms.jpg" width="40%" height="20%"></img></br>Fantas&iacute;a
+		</div>
+		<div id="column3">
+			<img src = "Catalogo/Images cat2/120410crawlers.jpg" width="40%" height="20%"></img></br>Horror
+		</div>
+		<div id="column4">
+			<img src = "Catalogo/Images cat2/6148028.jpg" width="40%" 	height="20%"></img></br>Ciencia Ficci&oacute;n
+		</div>
+		<div id="column5">
+			<img src = "Catalogo/Images cat2/here-bad-cover.jpg" width="40%" height="20%"></img></br>J&oacute;venes Adultos
+		</div>
+		<div id="column6">
+			<img src = "Catalogo/Images cat2/SecretsofaWeddingNight-cover-hi-res.jpg" width="40%" height="20%"></img></br>Romance
+		</div>
+	</div>
+	<div id="row3" align="center">
+		<div id="column2">
+			<img src = "Catalogo/Images cat3/top1.jpg" width="40%" height="20%"></img></br>#1</div>
+		<div id="column3">
+			<img src = "Catalogo/Images cat3/top2.jpg" width="40%" height="20%"></img></br>#2</div>
+		<div id="column4">
+			<img src = "Catalogo/Images cat3/top3.jpg" width="40%" height="20%"></img></br>#3</div>
+		<div id="column5">
+			<img src = "Catalogo/Images cat3/top4.png" width="40%" height="20%"></img></br>#4</div>
+		<div id="column6">
+			<img src = "Catalogo/Images cat3/top5.jpg" width="40%" height="20%"></img></br>#5</div>
+	</div>
+</div>
+HERE
+?>
