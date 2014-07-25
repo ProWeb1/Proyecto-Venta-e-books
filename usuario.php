7583c40e1c22c12b3bc53b1f3a5a2f405baeb2ab
IvonneBurgos <irburgos@espol.edu.ec>
2014-07-24 23:24:15 -0500

    Cambio y Aumento en CSS PARA BIBLIO

---
+++ usuario.php
@@ -0,0 +1,42 @@
+<?php
+	
+	class usuario {
+		private $idusuario;
+		private $idpersona;
+		private $usuario;
+		private $clave;
+		const TABLA = 'usuario';
+
+		function __construct($idtraductor, $idpersona, $idgrupo){
+			$this->idusuario = $idtraductor;
+			$this->idpersona = $idpersona;
+			$this->idgrupo = $idgrupo;
+
+		}
+		
+		function getidtraductor(){
+			return $this->idtraductor;
+		}
+
+		function getidpersona(){
+			return $this->idpersona;
+		}
+		function getidgrupo(){
+			return $this->idgrupo;
+		}
+
+
+		function setidtraductor($idtraductor){
+			$this->idtraductor= $idtraductor;
+		}
+
+		function setidpersona($idpersona){
+			$this->idpersona = $idpersona;
+		}
+		function setidgrupo($idgrupo){
+			$this->idgrupo = $idgrupo;
+		}
+				
+	}
+
+?>
