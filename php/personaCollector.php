456de12e1db20f25bb5f4cab92ed7d7a822e5166
EdwinRobalino <edboroba@gmail.com>
2014-07-27 19:43:00 -0700

    Colector funcionando
    crea y guarda objetos

--- php/personaCollector.php
+++ php/personaCollector.php
@@ -1,37 +1,31 @@
 <?php
 //require_once 'coneccion.php';
 require 'persona.php';
-
+$counter = 0;
 class personaCollector{
 	private $presonaCollector = array();
 }
-      
+//CONEXION CON LA BD Y TABLA    
 $conn= mysql_connect("localhost","root", "root") or die (mysql_error());
 mysql_select_db("ebbbooks", $conn);
-$query = "SELECT * FROM persona"; 
-	 
+$query = "SELECT * FROM persona"; 	 
 $result = mysql_query($query) or die(mysql_error());
-//$result = $sql->query("SELECT * FROM `persona`;");
-/*
-for ($set = array (); $row = $result->fetch_row(); $set[] = $row){
-	print_r($set);
-}
+//**********************************
 
-//print_r($set);
-
-
- //PRUEBA DE QUE SI CREA PERSONAS
-echo "<br /> \n";
-$andy = new persona(919, "andy", "daniel", "jefferson", "primero", "1994-12-1", 0924953243, "M", "bla@", 911, "la luna", "guayaquil");
-print_r ($andy->getCodigo());
-
-*/
-while ($row = mysql_fetch_row($result)){
-	/*for($i=0; $i<count($row); $i++){
-		echo $row[$i];
-		echo "<br />";
-	}*/
+while ($row = mysql_fetch_row($result)){ //LEE UNO DE LOS ELEMENTOS DE LA TABLA CON CADA VUELTA
+	//CREA UNA NUEVA PERSONA CON LOS DATOS DE ROW
 	$per = new persona($row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9],$row[10],$row[11]);
+	//ASIGNA LA PERSONA AL ARREGLO DEL COLECTOR
+	$personaCollector[$counter] = $per;
+	$counter++;
 }
 
+//LEYENDO DATOS DEL COLECTOR
+echo "REGISTROS DENTRO DEL COLECTOR <br />";
+for ($i=0; $i<count($personaCollector); $i++){
+	echo "Codigo: ".$personaCollector[$i]->getCodigo()." <br />";
+	echo "Nombre: ".$personaCollector[$i]->getPrimerNombre()." <br />";
+	echo "Apellido: ".$personaCollector[$i]->getPrimerApellido()." <br />";
+	echo "<br />";
+}
 ?>
