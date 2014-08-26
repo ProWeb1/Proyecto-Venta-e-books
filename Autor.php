d38f9a246d5722ef1a91ae5f2812b1fcf3f24579
IvonneBurgos <irburgos@espol.edu.ec>
2014-08-25 20:57:10 -0500

    Carga Dinámica de Autores

---
+++ Autor.php
@@ -0,0 +1,38 @@
+<?php
+
+class Autor
+{
+    private $idAutor;   
+	private $nombreCompleto;
+	private $foto;
+   
+    
+     function __construct($idAutor, $nombreCompleto,$foto) {
+       self::setIdAutor($idAutor);
+       self::setnombreCompleto($nombreCompleto);
+	   self::setfoto($foto);
+	 
+     }
+    
+     function setIdAutor($idAutor){
+       $this->idAutor = $idAutor;
+     } 
+     function getIdAutor(){
+       return $this->idAutor;
+     }
+     function setnombreCompleto($nombreCompleto){
+       $this->nombreCompleto = $nombreCompleto;
+     } 
+     function getnombreCompleto(){
+       return $this->nombreCompleto;
+     } 
+	 
+   function setFoto($foto){
+       $this->foto = $foto;
+     } 
+     function getFoto(){
+       return $this->foto;
+     } 
+}
+
+?> 
