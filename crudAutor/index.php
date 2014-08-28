0e4f3f39083aaa9a1f7dd481de6d9f4dc4566497
IvonneBurgos <irburgos@espol.edu.ec>
2014-08-27 21:38:10 -0500

    AdminCrud
    Interfaz y enlace

---
+++ crudAutor/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+<?php
+include_once("AutorCollector.php");
+$AutorCollector = new AutorCollector();
+?>
+<body>
+<div id="main">
+<table>
+<tr><td><a href="formularioAutorInsert.php">Nuevo</a></td></tr>
+<?php
+foreach ($AutorCollector->readAutor() as $c){
+  echo "<tr>";
+  echo "<td>".$c->getIdAutor() ."</td>";
+  echo "<td>".$c->getnombreCompleto()."</td>";
+  echo "<td><a href='formularioAutorEditar.php?id=".$c->getIdAutor()."'>editar</a></td>";
+  echo "<td><a href='eliminar.php?id=".$c->getIdAutor()."'>eliminar</a></td>"; 
+  echo "</tr>"; 
+}
+?>
+</table>
+</div>
+</body>
+</html>
