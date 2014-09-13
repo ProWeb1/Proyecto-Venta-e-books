c25f9fa10266c96acfbbb6b362c8b47b69638b85
María José Bravo Macías <majitobravomacias@gmail.com>
2014-09-13 15:44:53 -0500

    identaciòn y remover codigo php que no se utiliza

--- perfilAdmin.php
+++ perfilAdmin.php
@@ -61,127 +61,71 @@ if($_SESSION['actualmente_ingresado'] == 1){
       <div class="row">
         <div class="col-md-10  col-md-offset-1">
         
-    <div class="col-md-2 col-sm-3 col-xs-6">
-    <p>eBook</p>
-    <a href="crudEbook/index.php" onclick="location.href='login.htm'"><i class="fa fa-book fa-5x pull-left fa-border"></i></a>
+          <div class="col-md-2 col-sm-3 col-xs-6">
+            <p>eBook</p>
+            <a href="crudEbook/index.php" onclick="location.href='login.htm'"><i class="fa fa-book fa-5x pull-left fa-border"></i></a>
+          </div>
+         
+          <div class="col-md-2 col-sm-3 col-xs-6">
+            <p>Autor</p>
+            <a href="crudAutor/index.php" onclick="location.href='login.htm'"><i class="fa fa-pencil fa-5x pull-left fa-border"></i></a>
+          </div>
+          <div class="col-md-2 col-sm-3 col-xs-6">
+            <p>DUMB TEXT</p>
+            <a href=" " onclick="location.href='login.htm'"><i class="fa fa-user fa-5x pull-left fa-border"></i></a>
+          </div>
+          <div class="col-md-2 col-sm-3 col-xs-6">
+            <p>DUMB TEXT</p>
+            <a href="crudUsuario/indexUsuario.php " onclick="location.href='login.htm'"><i class="fa fa-user fa-5x pull-left fa-border"></i></a>
+          </div>
+          <div class="col-md-2 col-sm-3 col-xs-6">
+            <p>Usuario</p>
+            <a href="crudUsuario/indexUsuario.php " onclick="location.href='login.htm'"><i class="fa fa-user fa-5x pull-left fa-border"></i></a>
+          </div>
+          <div class="col-md-2 col-sm-3 col-xs-6">
+            <p>Colección</p>
+            <a href=" " onclick="location.href='login.htm'"><i class="fa fa-list-ol fa-5x pull-left fa-border"></i></a>
+          </div> 
+        </div><!-- Final Fila -->
+      </div>
     </div>
-   
-   <div class="col-md-2 col-sm-3 col-xs-6">
-<p>Autor</p>
-    <a href="crudAutor/index.php" onclick="location.href='login.htm'"><i class="fa fa-pencil fa-5x pull-left fa-border"></i></a>
-    
-   </div>
-   <div class="col-md-2 col-sm-3 col-xs-6">
-<p>DUMB TEXT</p>
-   <a href=" " onclick="location.href='login.htm'"><i class="fa fa-user fa-5x pull-left fa-border"></i></a>
-    
-   </div>
-   <div class="col-md-2 col-sm-3 col-xs-6">
-<p>DUMB TEXT</p>
-  <a href="crudUsuario/indexUsuario.php " onclick="location.href='login.htm'"><i class="fa fa-user fa-5x pull-left fa-border"></i></a>
-    
-   </div>
-   <div class="col-md-2 col-sm-3 col-xs-6">
-<p>Usuario</p>
-   <a href="crudUsuario/indexUsuario.php " onclick="location.href='login.htm'"><i class="fa fa-user fa-5x pull-left fa-border"></i></a>
-    
-   </div>
-   <div class="col-md-2 col-sm-3 col-xs-6">
-<p>Colección</p>
-   <a href=" " onclick="location.href='login.htm'"><i class="fa fa-list-ol fa-5x pull-left fa-border"></i></a>
-   </div> 
-      </div><!-- Final Fila -->
-    </div>
-        </div>
-
-
-     <!-- Catalogo -->
-     <div id="work" class="pagina">
-         <div class="container-fluid">
-  <div class="col-md-12 ">
-  <div class="gale">
-    
-      <?
-
-include_once("ebooksElegidosCollector.php");
-
-       $arrayColeccion1= array();
-       $ColeccionCollectorObj = new ebooksElegidosCollector();
-       $directory="images/portadas";
-       $dirint = dir($directory);
-       foreach ($ColeccionCollectorObj->librosUsuario() as $a){
-        $aux1 = $a; 
-        array_push($arrayColeccion1, $aux1);
-       
-}
- 
-    while (($archivo = $dirint->read()) !== false){
-      if (eregi("gif", $archivo) || eregi("jpg", $archivo) || eregi("png", $archivo)){
-        $compara = ($directory."/".$archivo);
-      for($i = 0; $i < sizeof($arrayColeccion1);$i++){
-      if ($compara == $arrayColeccion1[$i]){
-
-            echo '<div class="col-md-2 col-sm-3 col-xs-6">';
-  echo '<a href="#">';
-            echo '<img  class="img-responsive" src="'.$directory."/".$archivo.'">';
-      
-      
- echo '</a>';
-echo '</div>';
-}
-      }
-    }
-  }
- $dirint->close(); 
-?>
-</div>
-</div>
-</div>
-</div>
     <!-- Footer -->
     <footer class="bg-primary">
       <div class="container">
         <div class="row">
-			<div class="col-md-4 ">
-				<div class="constr cp-right">
-					<p>&copy; 2014 <a href="http://ebbbooks.com">eBBBOOKS</a>. All Rights Reserved. </p>
-				</div><!-- end contr -->
-			</div><!-- Final de Columna -->
-			<div class="col-md-4 text-center">
-			</div>
-			<div class="col-md-4 text-right">
-			<div class="contr">
-			<ul class="list-inline">
-			<li><a href="https://www.facebook.com/" class="socIcon" data-toggle="tooltip" title="Facebook" data-original-title="Facebook" data-gal='tooltip' data-placement="top"><i class="fa fa-facebook fa-2x"></i></a></li>
-			<li><a href="https://twitter.com/" class="socIcon" data-toggle="tooltip" title="Twitter" data-original-title="Twitter" data-gal='tooltip' data-placement="top"><i class="fa fa-twitter fa-2x"></i></a></li>
-			<li><a href="http://www.youtube.com/" class="socIcon" data-toggle="tooltip" title="Youtube" data-original-title="Youtube" data-gal='tooltip' data-placement="top"><i class="fa fa-youtube fa-2x"></i></a></li>
-			</ul>
-			</div>
-			</div>
+    			<div class="col-md-4 ">
+     				<div class="constr cp-right">
+     					<p>&copy; 2014 <a href="http://ebbbooks.com">eBBBOOKS</a>. All Rights Reserved. </p>
+     				</div><!-- end contr -->
+     			</div><!-- Final de Columna -->
+    		  <div class="col-md-4 text-center">
+          </div>
+      		<div class="col-md-4 text-right">
+      			<div class="contr">
+      		  	<ul class="list-inline">
+      			 <li><a href="https://www.facebook.com/" class="socIcon" data-toggle="tooltip" title="Facebook" data-original-title="Facebook" data-gal='tooltip' data-placement="top"><i class="fa fa-facebook fa-2x"></i></a></li>
+      			 <li><a href="https://twitter.com/" class="socIcon" data-toggle="tooltip" title="Twitter" data-original-title="Twitter" data-gal='tooltip' data-placement="top"><i class="fa fa-twitter fa-2x"></i></a></li>
+      			 <li><a href="http://www.youtube.com/" class="socIcon" data-toggle="tooltip" title="Youtube" data-original-title="Youtube" data-gal='tooltip' data-placement="top"><i class="fa fa-youtube fa-2x"></i></a></li>
+      			 </ul>
+      		  </div>
+          </div>
         </div>
       </div>
     </footer>
     <!-- /Footer -->
-
     <!-- JavaScript -->
-
     <script src="js/jquery-1.10.2.js"></script>
     <script src="js/bootstrap.js"></script>
-   
     <script src="js/modernizr.js"></script>
     <script src="js/jquery.sticky.js" ></script>
     <script src="js/jquery.quicksand.js" ></script>
-
-	  <script src="js/jquery.nav.js"></script>
+    <script src="js/jquery.nav.js"></script>
     <script src="js/jquery.scrollTo.js" ></script>
-    
     <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
     <script src="js/script.js"></script>
-
     <!-- Javascript para menu y srolling -->
-
   </div>
-  </body>
+</body>
 </html>
 <?php } else {?>
 <!DOCTYPE html>
